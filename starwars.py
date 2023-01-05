971dbfac38b6cb873a813bb4a322132ddc4138cc
louise delauney <louisedelauney@gmail.com>
2023-01-05 12:03:37 +0100

    first commit

---
+++ starwars.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+
+st.title('Star Wars')
+
+req = requests.get("https://swapi.dev/api/")
+
+attributes = req.json()
+
+select_attribute = st.selectbox(
+    'choose a category :',
+    attributes
+)
+
+searched = st.text_input("Search")
+
+new_url = f"{attributes[select_attribute]}?search={searched}"
+
+new_req = requests.get(new_url)
+
+result = new_req.json()["results"]
+st.write("Résultats")
+
+for res in result:
+    st.markdown("- " + res["name"])
+
+st.snow()
+st.balloons()
