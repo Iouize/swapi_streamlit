f665afe4a17c7dc3230f1fe381cc76d88e076691
louise delauney <louisedelauney@gmail.com>
2023-01-05 16:50:05 +0100

    display first results without search bar

--- starwars.py
+++ starwars.py
@@ -3,29 +3,44 @@ import pandas as pd
 import numpy as np
 import requests
 
+def print_results(result, attribute):
+    if attribute != "films":
+        for res in result:
+            st.markdown("- " + res["name"])
+    else:
+        for res in result:
+            st.markdown("- " + res["title"])
+
 st.title('Star Wars')
 
 req = requests.get("https://swapi.dev/api/")
-
 attributes = req.json()
 
-select_attribute = st.selectbox(
-    'choose a category :',
-    attributes
-)
+col1, col2 = st.columns(2)
 
-searched = st.text_input("Search")
+with col1:
+    select_attribute = st.selectbox(
+        'choose a category :',
+        attributes
+    )
+
+with col2:
+    searched = st.text_input("Search")
+
 
 new_url = f"{attributes[select_attribute]}?search={searched}"
+new_req = requests.get(new_url)
 
 if searched != "":
-    new_req = requests.get(new_url)
-
     result = new_req.json()["results"]
     st.write("Résultats")
+    print_results(result, select_attribute)
 
-    for res in result:
-        st.markdown("- " + res["name"])
+else:
+    stantard_req = requests.get(attributes[select_attribute])
+    stantard_res = stantard_req.json()["results"]
+    print_results(stantard_res, select_attribute)
+
 
 st.snow()
 st.balloons()
