676b73ec622599dabd63dd98c37c82fccac705a6
louise delauney <louisedelauney@gmail.com>
2023-01-06 10:38:12 +0100

    factorisation

--- starwars.py
+++ starwars.py
@@ -3,44 +3,62 @@ import pandas as pd
 import numpy as np
 import requests
 
-def print_results(result, attribute):
-    if attribute != "films":
-        for res in result:
-            st.markdown("- " + res["name"])
-    else:
-        for res in result:
-            st.markdown("- " + res["title"])
+def generate_url(url, category, searched):
+    return f"{url}{category}?search={searched}"
 
-st.title('Star Wars')
+def make_request(url):
+    return requests.get(url)
 
-req = requests.get("https://swapi.dev/api/")
-attributes = req.json()
-
-col1, col2 = st.columns(2)
-
-with col1:
-    select_attribute = st.selectbox(
-        'choose a category :',
-        attributes
+def choose_category(url):
+    categories = make_request(url).json()
+    category = st.selectbox(
+        'Choose a category :',
+        categories
     )
+    return category
 
-with col2:
-    searched = st.text_input("Search")
+def generate_url_pages_results(req, url):
+    nb_pages = req.json()['count'] // 10
+    if req.json()['count'] > nb_pages * 10 :
+        nb_pages += 1
+    link_pages = []
+    for i in range(nb_pages):
+        link_pages.append(f"{url}&page={i+1}")
+    return link_pages
 
+def print_results(list_url, category):
+    results = []
+    for url in list_url:
+        req = requests.get(url)
+        for res in req.json()["results"]:
+            if category != "films":
+                results.append(res["name"])
+            else:
+                results.append(res["title"])
+    return results
 
-new_url = f"{attributes[select_attribute]}?search={searched}"
-new_req = requests.get(new_url)
+def display_site():
+    url = "https://swapi.dev/api/"
 
-if searched != "":
-    result = new_req.json()["results"]
-    st.write("Résultats")
-    print_results(result, select_attribute)
+    st.title('Star Wars')
 
-else:
-    stantard_req = requests.get(attributes[select_attribute])
-    stantard_res = stantard_req.json()["results"]
-    print_results(stantard_res, select_attribute)
+    col1, col2 = st.columns(2)
 
+    with col1:
+        category = choose_category(url)
 
-st.snow()
-st.balloons()
+    with col2:
+        searched = st.text_input("Search")
+
+    new_url = generate_url(url, category, searched)
+    req = make_request(new_url)
+
+    list_pages = generate_url_pages_results(req, new_url)
+    list_results = print_results(list_pages, category)
+
+    st.write(f"We found {len(list_results)} result(s) :")
+
+    for result in list_results:
+        st.markdown("- " + result)
+
+display_site()
